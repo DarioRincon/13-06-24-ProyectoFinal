e08cf4817f2e0118ec62c78ebc199e2c0aad473e
GAMER <GAMER@DESKTOP-JU92E3V>
2024-06-13 14:19:34 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProyectoFinal
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnGenerarResultado;
+        private System.Windows.Forms.Button btnMostrarHistorial;
+        private System.Windows.Forms.Button btnBorrarHistorial;
+        private System.Windows.Forms.ListBox lstHistorial;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnGenerarResultado = new System.Windows.Forms.Button();
+            this.btnMostrarHistorial = new System.Windows.Forms.Button();
+            this.btnBorrarHistorial = new System.Windows.Forms.Button();
+            this.lstHistorial = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerarResultado
+            // 
+            this.btnGenerarResultado.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnGenerarResultado.Location = new System.Drawing.Point(12, 199);
+            this.btnGenerarResultado.Name = "btnGenerarResultado";
+            this.btnGenerarResultado.Size = new System.Drawing.Size(150, 23);
+            this.btnGenerarResultado.TabIndex = 0;
+            this.btnGenerarResultado.Text = "Generar Resultado";
+            this.btnGenerarResultado.UseVisualStyleBackColor = false;
+            this.btnGenerarResultado.Click += new System.EventHandler(this.btnGenerarResultado_Click);
+            // 
+            // btnMostrarHistorial
+            // 
+            this.btnMostrarHistorial.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnMostrarHistorial.Location = new System.Drawing.Point(168, 199);
+            this.btnMostrarHistorial.Name = "btnMostrarHistorial";
+            this.btnMostrarHistorial.Size = new System.Drawing.Size(150, 23);
+            this.btnMostrarHistorial.TabIndex = 1;
+            this.btnMostrarHistorial.Text = "Mostrar Historial";
+            this.btnMostrarHistorial.UseVisualStyleBackColor = false;
+            this.btnMostrarHistorial.Click += new System.EventHandler(this.btnMostrarHistorial_Click);
+            // 
+            // btnBorrarHistorial
+            // 
+            this.btnBorrarHistorial.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnBorrarHistorial.Location = new System.Drawing.Point(86, 228);
+            this.btnBorrarHistorial.Name = "btnBorrarHistorial";
+            this.btnBorrarHistorial.Size = new System.Drawing.Size(150, 23);
+            this.btnBorrarHistorial.TabIndex = 2;
+            this.btnBorrarHistorial.Text = "Borrar Historial";
+            this.btnBorrarHistorial.UseVisualStyleBackColor = false;
+            this.btnBorrarHistorial.Click += new System.EventHandler(this.btnBorrarHistorial_Click);
+            // 
+            // lstHistorial
+            // 
+            this.lstHistorial.FormattingEnabled = true;
+            this.lstHistorial.Location = new System.Drawing.Point(12, 33);
+            this.lstHistorial.Name = "lstHistorial";
+            this.lstHistorial.Size = new System.Drawing.Size(306, 160);
+            this.lstHistorial.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "KickLogicStats";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(330, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstHistorial);
+            this.Controls.Add(this.btnBorrarHistorial);
+            this.Controls.Add(this.btnMostrarHistorial);
+            this.Controls.Add(this.btnGenerarResultado);
+            this.Name = "Form1";
+            this.Text = "Resultados de Partidos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ProyectoFinal/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class Form1 : Form
+    {
+        private List<string> equipos = new List<string> { "Equipo A", "Equipo B", "Equipo C", "Equipo D" };
+        private Dictionary<string, int> puntajes = new Dictionary<string, int>();
+        private List<string> historialResultados = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarPuntajes();
+        }
+
+        private void InicializarPuntajes()
+        {
+            foreach (var equipo in equipos)
+            {
+                puntajes[equipo] = 0;
+            }
+        }
+
+        private void btnGenerarResultado_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            var equipo1 = equipos[random.Next(equipos.Count)];
+            var equipo2 = equipos[random.Next(equipos.Count)];
+
+            // Asegurarse de que no sean el mismo equipo
+            while (equipo1 == equipo2)
+            {
+                equipo2 = equipos[random.Next(equipos.Count)];
+            }
+
+            int puntaje1 = random.Next(0, 6);
+            int puntaje2 = random.Next(0, 6);
+
+            string resultado = $"{equipo1} {puntaje1} - {puntaje2} {equipo2}";
+            historialResultados.Add(resultado);
+
+            // Sumar puntos
+            if (puntaje1 > puntaje2)
+            {
+                puntajes[equipo1] += 3;
+            }
+            else if (puntaje1 < puntaje2)
+            {
+                puntajes[equipo2] += 3;
+            }
+            else
+            {
+                puntajes[equipo1] += 1;
+                puntajes[equipo2] += 1;
+            }
+
+            MessageBox.Show(resultado);
+        }
+
+        private void btnMostrarHistorial_Click(object sender, EventArgs e)
+        {
+            lstHistorial.Items.Clear();
+            foreach (var resultado in historialResultados)
+            {
+                lstHistorial.Items.Add(resultado);
+            }
+
+            lstHistorial.Items.Add("Puntajes:");
+            foreach (var equipo in equipos)
+            {
+                lstHistorial.Items.Add($"{equipo}: {puntajes[equipo]} puntos");
+            }
+        }
+
+        private void btnBorrarHistorial_Click(object sender, EventArgs e)
+        {
+            historialResultados.Clear();
+            lstHistorial.Items.Clear();
+            InicializarPuntajes();
+            MessageBox.Show("El historial ha sido borrado.");
+        }
+    }
+}
